c5947bce3058902215cda2b3064312c0c812059d
mbhesam <m.b.hesam>
2023-02-23 15:02:45 +0330

    make  user model better

--- users/models.py
+++ users/models.py
@@ -1,22 +1,69 @@
 from django.db import models
-from django.contrib.auth.models import UserManager
-from django.contrib.auth.base_user import AbstractBaseUser
-#from mongoengine import Document, fields
-class User(AbstractBaseUser):
-    email = models.EmailField(("email address"), blank=True)
-    objects = UserManager()
-
-    EMAIL_FIELD = "email"
-    REQUIRED_FIELDS = ["email"]
-
-    def clean(self):
-        super().clean()
-        self.email = self.__class__.objects.normalize_email(self.email)
-
-    def get_email(self):
-        """Return the short name for the user."""
-        return self.email
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import BaseUserManager as BUM
+from django.contrib.auth.models import PermissionsMixin
 
 
 
+class BaseUserManager(BUM):
+    def create_user(self, email, is_active=True, is_admin=False, password=None):
 
+        if not email:
+            raise ValueError("Users must have an email address")
+
+        user = self.model(email=self.normalize_email(email.lower()), is_active=is_active, is_admin=is_admin)
+
+        if password is not None:
+            user.set_password(password)
+        else:
+            user.set_unusable_password()
+
+        user.full_clean()
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password=None):
+
+        user = self.create_user(
+            email=email,
+            is_active=True,
+            is_admin=True,
+            password=password,
+        )
+
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+class BaseUser(AbstractBaseUser, PermissionsMixin):
+
+    firstname = models.CharField(
+        max_length=255,
+        unique=True,
+        help_text=('Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'),
+    )
+
+    lastname = models.CharField(
+        max_length=255,
+        unique=True,
+        help_text=('Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'),
+    )
+
+    email = models.EmailField(verbose_name = "email address",
+                              unique=True)
+
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = BaseUserManager()
+
+    USERNAME_FIELD = "email"
+
+    def __str__(self):
+        return self.firstname+" "+self.lastname
+
+    def is_staff(self):
+        return self.is_admin
