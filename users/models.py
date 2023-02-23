3a2de05e380883a8698c9b2f2a2099ed7a995ba4
mbhesam <m.b.hesam>
2023-02-23 12:52:37 +0330

    connect mongo to django with hard

--- users/models.py
+++ users/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import UserManager
+from django.contrib.auth.base_user import AbstractBaseUser
+#from mongoengine import Document, fields
+class User(AbstractBaseUser):
+    email = models.EmailField(("email address"), blank=True)
+    objects = UserManager()
 
-# Create your models here.
+    EMAIL_FIELD = "email"
+    REQUIRED_FIELDS = ["email"]
+
+    def clean(self):
+        super().clean()
+        self.email = self.__class__.objects.normalize_email(self.email)
+
+    def get_email(self):
+        """Return the short name for the user."""
+        return self.email
+
+
+
+
